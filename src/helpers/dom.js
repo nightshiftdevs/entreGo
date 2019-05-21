b9f4eb66121a6e89463d437e0a18eeeb2092563d
ByeBye-Sama <joaquin.byebye@gmail.com>
2019-05-20 22:27:49 -0500

    Add helpers for DOM events. Add style & effects to header.component

---
+++ src/helpers/dom.js
@@ -0,0 +1,39 @@
+export function hasClass(el, className) {
+  if (!el) return false
+  if (el.classList) {
+    return el.classList.contains(className)
+  } else {
+    return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className)
+  }
+}
+
+export function addClass(el, className) {
+  if (el) {
+    if (el.classList) {
+      el.classList.add(className)
+    } else {
+      el.className += ' ' + className
+    }
+  }
+}
+
+export function removeClass(el, className) {
+  if (el) {
+    if (el.classList) {
+      el.classList.remove(className)
+    } else {
+      el.className = el.className
+        .replace(
+          new RegExp(
+            '(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'
+          ), ' '
+        )
+    }
+  }
+}
+
+export function removeClassCollection(collection, className) {
+  for (let i = 0; i < collection.length; i++) {
+    removeClass(collection[i], className);
+  }
+}
