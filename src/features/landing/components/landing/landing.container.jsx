8f701d5b3c9a393a7e5a852c2b2a7018b457502c
ByeBye-Sama <joaquin.byebye@gmail.com>
2019-06-07 15:51:51 -0500

    Add footer and work now button

--- src/features/landing/components/landing/landing.container.jsx
+++ src/features/landing/components/landing/landing.container.jsx
@@ -143,6 +143,9 @@ class LandingContainer extends Component {
 
           <section className="work-with-us">
             <h2 className="title">W O R K &nbsp; W I T H &nbsp; U S</h2>
+            <div className="start-to-work">
+              <Link to="/register/driver" className="btn-work">S T A R T &nbsp; N O W !</Link>
+            </div>
             <div className="benefit">
               <div className="benefit-icon">
                 <FontAwesomeIcon icon={faDollarSign} />
